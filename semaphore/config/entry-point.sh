b165c17c1e7625b309e413a84d5dbfe706af077b
Dell Optiplex Mini <craddock9richard@gmail.com>
2024-01-07 00:25:58 -0500

    edit the entry script

--- semaphore/config/entry-point.sh
+++ semaphore/config/entry-point.sh
@@ -10,8 +10,8 @@ mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 # Modify config.json with the database name and password
 jq '.mysql.pass = env.MYSQL_ROOT_PASSWORD | .mysql.name = env.MYSQL_DATABASE' /opt/semaphore/config.json > /opt/semaphore/temp.json && mv /opt/semaphore/temp.json /opt/semaphore/config.json
 
-# Start Semaphore service
-semaphore service --config=/opt/semaphore/config.json
-
 # add admin user and password
 semaphore user add --admin --login $SEMAPHORE_ADMIN_USER_LOGIN --name $SEMAPHORE_ADMIN_USER_NAME --email $SEMAPHORE_ADMIN_USER_EMAIL --password $SEMAPHORE_ADMIN_USER_PASSWD
+
+# Start Semaphore service
+semaphore service --config=/opt/semaphore/config.json
