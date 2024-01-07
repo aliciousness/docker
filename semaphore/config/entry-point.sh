3bff09998ced28b1dd06cef17f64de9757387335
Dell Optiplex Mini <craddock9richard@gmail.com>
2024-01-07 00:21:53 -0500

    MAde some minor changes

--- semaphore/config/entry-point.sh
+++ semaphore/config/entry-point.sh
@@ -12,3 +12,6 @@ jq '.mysql.pass = env.MYSQL_ROOT_PASSWORD | .mysql.name = env.MYSQL_DATABASE' /o
 
 # Start Semaphore service
 semaphore service --config=/opt/semaphore/config.json
+
+# add admin user and password
+semaphore user add --admin --login $SEMAPHORE_ADMIN_USER_LOGIN --name $SEMAPHORE_ADMIN_USER_NAME --email $SEMAPHORE_ADMIN_USER_EMAIL --password $SEMAPHORE_ADMIN_USER_PASSWD
